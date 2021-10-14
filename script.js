41711f91919491d86a4aaffb8fb29ea461fa2ef5
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-10-14 11:32:48 +0000

    add random color functionality

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const colorArr = ['#9289FF','red','blue'];
+
 const fetchData = () => {
   $.getJSON('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?',(json) => {
     $('#text').text(json.quoteText);
     $('#author').text('- '+json.quoteAuthor);
     $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + json.quoteText + '" ' + json.quoteAuthor));
+    const color = colorArr[Math.floor(Math.random()*colorArr.length)];
+    $('body, #new-quote, #tweet-quote').css('background-color', color);
+    $('#text, #author, .repo').css('color', color);
   });
 }
 
