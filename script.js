79619f6e270eb32ea670f9d50b0d00364a896e31
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-10-14 08:41:37 +0000

    add footer

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-$('#new-quote').click(() => {
+const fetchData = () => {
   $.getJSON('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?',(json) => {
     $('#text').text(json.quoteText);
     $('#author').text('- '+json.quoteAuthor);
     $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + json.quoteText + '" ' + json.quoteAuthor));
-  })
+  });
+}
+
+$(document).ready(() => {
+  fetchData();
 })
+
+$('#new-quote').click(() => {
+  fetchData();
+});
